b1aaad458b54cf7e2371e4454123f3622dad1456
@Mohsens22 <mohsens2@outlook.com>
2017-09-30 17:46:18 +0330

    Still not working

---
+++ Backtory/Backtory/ConnectionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backtory
+{
+    public class ConnectionHelper
+    {
+        public static async Task Initialize(string AuthID, string AuthMasterKey, string username, string password, string storageID)
+        {
+            var httpClient = new HttpClient();
+            MultipartFormDataContent form = new MultipartFormDataContent();
+            form.Add(new StringContent(username), "username");
+            form.Add(new StringContent(password), "password");
+
+            httpClient.DefaultRequestHeaders.Add("X-Backtory-Authentication-Id", AuthID);
+            httpClient.DefaultRequestHeaders.Add("X-Backtory-Authentication-Key", AuthMasterKey);
+            HttpResponseMessage response = await httpClient.PostAsync("https://api.backtory.com/auth/login", form);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
