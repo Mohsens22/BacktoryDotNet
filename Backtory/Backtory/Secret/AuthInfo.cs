483e7afbeea622bdf1a6b4544c9d60adf78b955d
@Mohsens22 <mohsens2@outlook.com>
2017-09-30 14:17:24 +0330

    Added initial methods

---
+++ Backtory/Backtory/Secret/AuthInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backtory.Secret
+{
+    internal class AuthInfo
+    {
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AuthID { get; set; }
+        public string AuthKey { get; set; }
+
+    }
+}
